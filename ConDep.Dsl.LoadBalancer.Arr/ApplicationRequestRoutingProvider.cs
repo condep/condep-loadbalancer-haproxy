61e99e5260987266a0ad5b4804fb0796320a7ec8
Jon Arild T�rresdal <jon@torresdal.net>
2012-09-17 23:41:15 +0200

    Restructured projects and solution. Changed build script to genereta nuspec files fro 3 packages: ConDep, ConDep.dsl and ConDep.Dsl.LoadBalancer.Arr.

---
+++ ConDep.Dsl.LoadBalancer.Arr/ApplicationRequestRoutingProvider.cs
@@ -0,0 +1,52 @@
+using ConDep.Dsl;
+using ConDep.Dsl.LoadBalancer;
+using ConDep.Dsl.WebDeploy;
+
+namespace ConDep.LoadBalancer.Arr
+{
+    public class ApplicationRequestRoutingProvider : WebDeployCompositeProviderBase
+    {
+        private readonly LoadBalanceState _state;
+        private readonly string _serverNameToChangeStateOn;
+
+        public ApplicationRequestRoutingProvider(LoadBalanceState state, string serverNameToChangeStateOn)
+        {
+            _state = state;
+            _serverNameToChangeStateOn = serverNameToChangeStateOn;
+        }
+
+        public override bool IsValid(Notification notification)
+        {
+            return true;
+        }
+
+        public override void Configure(DeploymentServer arrServer)
+        {
+            var condition = WebDeployExecuteCondition<ProvideForInfrastructure>.IsFailure(p => p.PowerShell("import-module ApplicationRequestRouting"));
+
+            DeployPsCmdLet(arrServer, condition);
+            Execute(_state, arrServer, _serverNameToChangeStateOn);
+            RemovePsCmdLet(arrServer, condition);
+        }
+
+        private void DeployPsCmdLet(DeploymentServer server, WebDeployExecuteCondition<ProvideForInfrastructure> condition)
+        {
+            Configure<ProvideForDeployment, ProvideForInfrastructure>(server, p => p.CopyDir(@"C:\GitHub\ConDep\ConDep.PowerShell.ApplicationRequestRouting\bin\Release", @"%temp%\ApplicationRequestRouting"), condition);
+        }
+
+        private void Execute(LoadBalanceState state, DeploymentServer server, string serverNameToChangeStateOn)
+        {
+            Configure<ProvideForInfrastructure>(server, p => p.PowerShell(string.Format(@"import-module $env:temp\ApplicationRequestRouting; Set-WebFarmServerState -State {0} -Name {1} -UseDnsLookup;", state.ToString(), serverNameToChangeStateOn), o => o.WaitIntervalInSeconds(10)));
+        }
+
+        private void RemovePsCmdLet(DeploymentServer arrServer, WebDeployExecuteCondition<ProvideForInfrastructure> condition)
+        {
+            Configure<ProvideForInfrastructure, ProvideForInfrastructure>(arrServer, p => p.PowerShell(@"remove-item $env:temp\ApplicationRequestRouting -force -recurse"), condition);
+        }
+
+        private void Condition(ProvideForInfrastructure c)
+        {
+            c.PowerShell("import-module ApplicationRequestRouting");
+        }
+    }
+}
