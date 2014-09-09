00a6b9d8d39d31f64f0e41074b1ea21bbd15abe4
Jon Arild Torresdal <jon@torresdal.net>
2014-09-09 14:29:45 +0200

    Created load balancr for ALOHA HAProxy

---
+++ ConDep.Dsl.LoadBalancer.AlohaHaProxy.Tests/Test.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Collections.Generic;
+using SnmpSharpNet;
+
+namespace SnmpTable
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                Console.WriteLine("Syntax: SnmpTable.exe <host> <community> <table oid>");
+                return;
+            }
+            Dictionary<String, Dictionary<uint, AsnType>> result = new Dictionary<String, Dictionary<uint, AsnType>>();
+            // Not every row has a value for every column so keep track of all columns available in the table
+            List<uint> tableColumns = new List<uint>();
+            // Prepare agent information
+            AgentParameters param = new AgentParameters(SnmpVersion.Ver2, new OctetString(args[1]));
+            IpAddress peer = new IpAddress(args[0]);
+            if (!peer.Valid)
+            {
+                Console.WriteLine("Unable to resolve name or error in address for peer: {0}", args[0]);
+                return;
+            }
+            UdpTarget target = new UdpTarget((IPAddress)peer);
+            // This is the table OID supplied on the command line
+            Oid startOid = new Oid(args[2]);
+            // Each table OID is followed by .1 for the entry OID. Add it to the table OID
+            startOid.Add(1); // Add Entry OID to the end of the table OID
+            // Prepare the request PDU
+            Pdu bulkPdu = Pdu.GetBulkPdu();
+            bulkPdu.VbList.Add(startOid);
+            // We don't need any NonRepeaters
+            bulkPdu.NonRepeaters = 0;
+            // Tune MaxRepetitions to the number best suited to retrive the data
+            bulkPdu.MaxRepetitions = 100;
+            // Current OID will keep track of the last retrieved OID and be used as 
+            //  indication that we have reached end of table
+            Oid curOid = (Oid)startOid.Clone();
+            // Keep looping through results until end of table
+            while (startOid.IsRootOf(curOid))
+            {
+                SnmpPacket res = null;
+                try
+                {
+                    res = target.Request(bulkPdu, param);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Request failed: {0}", ex.Message);
+                    target.Close();
+                    return;
+                }
+                // For GetBulk request response has to be version 2
+                if (res.Version != SnmpVersion.Ver2)
+                {
+                    Console.WriteLine("Received wrong SNMP version response packet.");
+                    target.Close();
+                    return;
+                }
+                // Check if there is an agent error returned in the reply
+                if (res.Pdu.ErrorStatus != 0)
+                {
+                    Console.WriteLine("SNMP agent returned error {0} for request Vb index {1}",
+                                      res.Pdu.ErrorStatus, res.Pdu.ErrorIndex);
+                    target.Close();
+                    return;
+                }
+                // Go through the VbList and check all replies
+                foreach (Vb v in res.Pdu.VbList)
+                {
+                    curOid = (Oid)v.Oid.Clone();
+                    // VbList could contain items that are past the end of the requested table.
+                    // Make sure we are dealing with an OID that is part of the table
+                    if (startOid.IsRootOf(v.Oid))
+                    {
+                        // Get child Id's from the OID (past the table.entry sequence)
+                        uint[] childOids = Oid.GetChildIdentifiers(startOid, v.Oid);
+                        // Get the value instance and converted it to a dotted decimal
+                        //  string to use as key in result dictionary
+                        uint[] instance = new uint[childOids.Length - 1];
+                        Array.Copy(childOids, 1, instance, 0, childOids.Length - 1);
+                        String strInst = InstanceToString(instance);
+                        // Column id is the first value past <table oid>.entry in the response OID
+                        uint column = childOids[0];
+                        if (!tableColumns.Contains(column))
+                            tableColumns.Add(column);
+                        if (result.ContainsKey(strInst))
+                        {
+                            result[strInst][column] = (AsnType)v.Value.Clone();
+                        }
+                        else
+                        {
+                            result[strInst] = new Dictionary<uint, AsnType>();
+                            result[strInst][column] = (AsnType)v.Value.Clone();
+                        }
+                    }
+                    else
+                    {
+                        // We've reached the end of the table. No point continuing the loop
+                        break;
+                    }
+                }
+                // If last received OID is within the table, build next request
+                if (startOid.IsRootOf(curOid))
+                {
+                    bulkPdu.VbList.Clear();
+                    bulkPdu.VbList.Add(curOid);
+                    bulkPdu.NonRepeaters = 0;
+                    bulkPdu.MaxRepetitions = 100;
+                }
+            }
+            target.Close();
+            if (result.Count <= 0)
+            {
+                Console.WriteLine("No results returned.\n");
+            }
+            else
+            {
+                Console.Write("Instance");
+                foreach (uint column in tableColumns)
+                {
+                    Console.Write("\tColumn id {0}", column);
+                }
+                Console.WriteLine("");
+                foreach (KeyValuePair<string, Dictionary<uint, AsnType>> kvp in result)
+                {
+                    Console.Write("{0}", kvp.Key);
+                    foreach (uint column in tableColumns)
+                    {
+                        if (kvp.Value.ContainsKey(column))
+                        {
+                            Console.Write("\t{0} ({1})", kvp.Value[column].ToString(),
+                                              SnmpConstants.GetTypeName(kvp.Value[column].Type));
+                        }
+                        else
+                        {
+                            Console.Write("\t-");
+                        }
+                    }
+                    Console.WriteLine("");
+                }
+            }
+        }
+        public static string InstanceToString(uint[] instance)
+        {
+            StringBuilder str = new StringBuilder();
+            foreach (uint v in instance)
+            {
+                if (str.Length == 0)
+                    str.Append(v);
+                else
+                    str.AppendFormat(".{0}", v);
+            }
+            return str.ToString();
+        }
+    }
+}
