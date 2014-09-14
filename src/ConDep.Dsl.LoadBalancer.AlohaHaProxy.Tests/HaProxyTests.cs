6ce6b434fe8bd7844a6f20419beacdbedba4b78a
Jon Arild Torresdal <jon@torresdal.net>
2014-09-14 21:15:55 +0200

    finished up load balancer

---
+++ src/ConDep.Dsl.LoadBalancer.AlohaHaProxy.Tests/HaProxyTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConDep.Dsl.Config;
+using NUnit.Framework;
+
+namespace ConDep.Dsl.LoadBalancer.AlohaHaProxy.Tests
+{
+    [TestFixture]
+    public class HaProxyTests
+    {
+        private string _json =
+    @"{
+    ""LoadBalancer"": 
+    {
+        ""Name"": ""jat-nlb01"",
+        ""Provider"": ""ConDep.Dsl.LoadBalancer.AlohaHaProxy.dll"",
+        ""UserName"": ""nlbUser"",
+        ""Password"": ""verySecureP@ssw0rd"",
+        ""Mode"": ""Sticky"",
+		""SuspendMode"" : ""Graceful"",
+        ""CustomConfig"" :
+        {
+            ""SnmpEndpoint"" : ""10.0.0.1""
+        }
+    },
+	""Servers"":
+    [
+        {
+            ""Name"" : ""jat-web01"",
+            ""LoadBalancerFarm"": ""farm1"",
+        },
+        {
+            ""Name"" : ""jat-web02"",
+            ""LoadBalancerFarm"": ""farm1"",
+        }
+    ],
+    ""DeploymentUser"": 
+    {
+        ""UserName"": ""torresdal\\condepuser"",
+        ""Password"": ""verySecureP@ssw0rd""
+    }
+}";
+
+        private ConDepEnvConfig _config;
+
+        [SetUp]
+        public void Setup()
+        {
+            var memStream = new MemoryStream(Encoding.UTF8.GetBytes(_json));
+
+            var parser = new EnvConfigParser();
+            _config = parser.GetTypedEnvConfig(memStream, null);
+        }
+
+        [Test]
+        public void TestThat_ConfigurationCanBeLoaded()
+        {
+            var tmp = "";
+        }
+    }
+}
--- src/ConDep.Dsl.LoadBalancer.AlohaHaProxy.Tests/Properties/AssemblyInfo.cs
+++ src/ConDep.Dsl.LoadBalancer.AlohaHaProxy.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConDep.Dsl.LoadBalancer.AlohaHaProxy")]
+[assembly: AssemblyTitle("ConDep.Dsl.LoadBalancer.AlohaHaProxy.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConDep.Dsl.LoadBalancer.AlohaHaProxy")]
+[assembly: AssemblyProduct("ConDep.Dsl.LoadBalancer.AlohaHaProxy.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f3893e68-5c53-4389-ba9f-3b042f94c8cf")]
+[assembly: Guid("996d93e9-c753-4bc5-903e-e1bc1eeec18a")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.31.0")]
-[assembly: AssemblyFileVersion("3.0.31.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
