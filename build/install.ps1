faf15018d825a2990414274927c81fb5db2b583a
Kjell Iver Breistøl <104171kjbr@Z63>
2016-12-08 19:49:44 +0100

    simplify assembly version setting and handles non-beta packages as well

--- build/install.ps1
+++ build/install.ps1
@@ -1,9 +1,8 @@
 ﻿#Setting version number in assemblies
 $appVeyorBuildVersion = $env:APPVEYOR_BUILD_VERSION
 
-$version = $appVeyorBuildVersion.Split('-') | Select-Object -First 1
-$buildNumber = $appVeyorBuildVersion.Split('-') | Select-Object -Last 1 | % {$_.replace("beta","")}
+$version = $appVeyorBuildVersion | % {$_.replace("-beta",".")}
 
-Set-AppveyorBuildVariable -Name "assembly_version" -Value "$version.$buildNumber"
-Set-AppveyorBuildVariable -Name "assembly_file_version" -Value "$version.$buildNumber"
-Set-AppveyorBuildVariable -Name "assembly_informational_version" -Value "$version.$buildNumber"
+Set-AppveyorBuildVariable -Name "assembly_version" -Value "$version"
+Set-AppveyorBuildVariable -Name "assembly_file_version" -Value "$version"
+Set-AppveyorBuildVariable -Name "assembly_informational_version" -Value "$version"
