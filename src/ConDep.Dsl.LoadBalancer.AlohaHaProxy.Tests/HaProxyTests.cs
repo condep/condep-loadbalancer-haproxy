9786430b0c84ad06b10b1c316523d3eadf6edbb6
Håkon Lerring <hakon.lerring@frende.no>
2017-10-26 15:40:17 +0200

    Add new GetServerState operation. This will allow us to check the server state when deciding which server to deploy to. Update condep-dsl

--- src/ConDep.Dsl.LoadBalancer.AlohaHaProxy.Tests/HaProxyTests.cs
+++ src/ConDep.Dsl.LoadBalancer.AlohaHaProxy.Tests/HaProxyTests.cs
@@ -1,65 +1,132 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ConDep.Dsl.Config;
+using ConDep.Dsl.Logging;
+using log4net;
+using log4net.Appender;
+using log4net.Core;
+using log4net.Repository.Hierarchy;
 using NUnit.Framework;
+using Logger = ConDep.Dsl.Logging.Logger;
 
 namespace ConDep.Dsl.LoadBalancer.AlohaHaProxy.Tests
 {
     [TestFixture]
     public class HaProxyTests
     {
-        private string _json =
-    @"{
-    ""LoadBalancer"": 
-    {
-        ""Name"": ""jat-nlb01"",
-        ""Provider"": ""ConDep.Dsl.LoadBalancer.AlohaHaProxy.dll"",
-        ""UserName"": ""nlbUser"",
-        ""Password"": ""verySecureP@ssw0rd"",
-        ""Mode"": ""Sticky"",
-		""SuspendMode"" : ""Graceful"",
-        ""CustomConfig"" :
+        class UnitTestLogger : Log4NetLoggerBase
         {
-            ""SnmpEndpoint"" : ""10.0.0.1""
-        }
-    },
-	""Servers"":
-    [
-        {
-            ""Name"" : ""jat-web01"",
-            ""LoadBalancerFarm"": ""farm1"",
-        },
-        {
-            ""Name"" : ""jat-web02"",
-            ""LoadBalancerFarm"": ""farm1"",
-        }
-    ],
-    ""DeploymentUser"": 
-    {
-        ""UserName"": ""torresdal\\condepuser"",
-        ""Password"": ""verySecureP@ssw0rd""
-    }
-}";
+            private readonly MemoryAppender _memAppender;
 
-        private ConDepEnvConfig _config;
+            public UnitTestLogger(ILog log4netLog, MemoryAppender memAppender) : base(log4netLog)
+            {
+                _memAppender = memAppender;
+            }
+
+            public override void Warn(string message, object[] formatArgs)
+            {
+            }
+
+            public override void Warn(string message, Exception ex, object[] formatArgs)
+            {
+            }
+
+            public override void Verbose(string message, object[] formatArgs)
+            {
+            }
+
+            public override void Verbose(string message, Exception ex, object[] formatArgs)
+            {
+            }
+
+            public override void Info(string message, object[] formatArgs)
+            {
+            }
+
+            public override void Info(string message, Exception ex, object[] formatArgs)
+            {
+            }
+
+            public override void Error(string message, object[] formatArgs)
+            {
+            }
+
+            public override void Error(string message, Exception ex, object[] formatArgs)
+            {
+            }
+
+            public override void Progress(string message, params object[] formatArgs)
+            {
+            }
+
+            public override void ProgressEnd()
+            {
+            }
+
+            public override void LogSectionStart(string name)
+            {
+            }
+
+            public override void LogSectionEnd(string name)
+            {
+            }
+
+            public LoggingEvent[] Events { get { return _memAppender.GetEvents(); } }
+
+            public void ClearEvents()
+            {
+                _memAppender.Clear();
+            }
+        }
+
+        private LoadBalancerConfig _config;
+        private UnitTestLogger CreateMemoryLogger()
+        {
+            var memAppender = new MemoryAppender { Name = "MemoryAppender" };
+            memAppender.ActivateOptions();
+
+            var repo = LogManager.GetRepository() as Hierarchy;
+            repo.Root.AddAppender(memAppender);
+            repo.Configured = true;
+            repo.RaiseConfigurationChanged(EventArgs.Empty);
+
+            return new UnitTestLogger(LogManager.GetLogger("root"), memAppender);
+        }
 
         [SetUp]
         public void Setup()
         {
-            var memStream = new MemoryStream(Encoding.UTF8.GetBytes(_json));
+            _config = new LoadBalancerConfig
+            {
 
-            //var parser = new EnvConfigParser();
-            //_config = parser.GetTypedEnvConfig(memStream, null);
+                Name = "loadbalancer",
+                Provider = "ConDep.Dsl.LoadBalancer.AlohaHaProxy.dll",
+                UserName = "username",
+                Password = "password",
+                Mode = "Sticky"
+            };
+            _config.CustomConfig = new ExpandoObject();
+
+            _config.CustomConfig.SnmpEndpoint = "endpointaddress";
+            _config.CustomConfig.Scope = "root";
+            _config.CustomConfig.SnmpPort = 161;
+            _config.CustomConfig.SnmpCommunity = "public";
+            _config.CustomConfig.WaitTimeInSecondsAfterSettingServerStateToOffline = 20;
+            _config.CustomConfig.WaitTimeInSecondsAfterSettingServerStateToOnline = 2;
+            Logger.Initialize(CreateMemoryLogger());
         }
 
         [Test]
-        public void TestThat_ConfigurationCanBeLoaded()
+        public void TestThat_StateCanBeFetched()
         {
-            var tmp = "";
+            var lb = new HaProxyLoadBalancer(_config);
+            var result = lb.GetServerState("server", "farm");
+            Assert.True(result == HaProxyLoadBalancer.ServerState.Online);
         }
     }
 }
